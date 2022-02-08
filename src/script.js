7540dd81cff346e7d20687544325b970bcf3f5c1
ritick <ritick.official@gmail.com>
2022-02-08 11:33:55 +0530

    updating changes

--- src/script.js
+++ src/script.js
@@ -1,36 +1,45 @@
 const chart = [];
-function productList() {
+function addProduct() {
   var id = document.getElementById("proID").value;
   var name = document.getElementById("proName").value;
   var price = document.getElementById("proPrice").value;
-  checkField(id, name, price);
+  var array = fetchData(id, name, price);
+  valid(id, name, price);
+  chart.push(array);
+  display(id, name, price);
 }
-function checkField(id, name, price) {
-  if ((id && name && price) != null) {
-    console.log(id, name, price);
-    var array = fetchData(id, name, price);
-    console.log(array);
-    console.log(chart);
-    chart.push(array);
-    var table = document.getElementById("tableData");
-    var row = table.insertRow(-1);
-    var box1 = row.insertCell(0);
-    var box2 = row.insertCell(1);
-    var box3 = row.insertCell(2);
-    for (var i = 0; i < chart.length; i++) {
-      box1.innerHTML = chart[i].productID;
-      box2.innerHTML = chart[i].productName;
-      box3.innerHTML = "USD " + chart[i].productPrice;
-    }
-  } else {
-    document.getElementById("list").innerHTML =
-      "Error !!! Please fill all the fields carefully.";
-  }
-}
+
 function fetchData(id, name, price) {
   return {
     productID: id,
     productName: name,
     productPrice: price,
   };
+}
+
+function valid(id, name, price) {
+  for (var i = 0; i <chart.length; i++) {
+    if (id == chart[i].productID) {
+      window.alert("This product ID already exist !!!");
+      return exit;
+    } else {
+      if (id.length == 0 || name.length == 0 || price.length == 0) {
+        window.alert("Please fill all the fields.");
+        return exit;
+      }
+    }
+  }
+}
+
+function display(id, name, price) {
+  var table = document.getElementById("tableData");
+  var row = table.insertRow(-1);
+  var box1 = row.insertCell(0);
+  var box2 = row.insertCell(1);
+  var box3 = row.insertCell(2);
+  for (var i = 0; i < chart.length; i++) {
+    box1.innerHTML = chart[i].productID;
+    box2.innerHTML = chart[i].productName;
+    box3.innerHTML = "USD " + chart[i].productPrice;
+  }
 }
